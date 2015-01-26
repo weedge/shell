a7db1d99f7143b9207dfe3359bd4743c09637846
weedge <weege@126.com>
2015-01-27 00:03:24 +0800

    Update cities.sh

--- cities.sh
+++ cities.sh
@@ -6,9 +6,7 @@ sh ./komovieCity.sh > komovie_cinema_cities.txt
 
 sh ./wangpiaoCity.sh > wangpiao_cinema_cities.txt
 
-cat wangpiao_cinema_cities.txt komovie_cinema_cities.txt meituan_cinema_cities.txt > cities.txt
-
-cat cities.txt | sort | uniq -c | awk -F' ' '{if($1>0)print $2}' > AllSiteCities.txt
+cat wangpiao_cinema_cities.txt komovie_cinema_cities.txt meituan_cinema_cities.txt | sort | uniq > AllSiteCities.txt
 
 
 

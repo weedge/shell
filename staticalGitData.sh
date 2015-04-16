e827e1622fc6a0837462920be69eb5a5d76ef481
weedge <weege007@gmail.com>
2015-04-16 19:24:18 +0800

    通过.git问价判断是否有git版本库

--- staticalGitData.sh
+++ staticalGitData.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #参数地址：http://segmentfault.com/a/1190000002434755
-if [ "$#" -eq 1 ] 2>/dev\null ;then  
-	cd $1
-
+	
+git=`ls -a | grep ^.git$`
+path=`pwd`
+	
+if [ "x$git" == "x" ] 2>/dev\null ;then  
+	echo -e "项目所在路径${path}中没有版本库git管理~!\n"
+else
 	#统计某人的代码提交量，包括增加，删除
 	echo -e "当前设置用户:$(git config --get user.name)的代码提交统计:\n"
 	git log --author="$(git config --get user.name)" --pretty=tformat: --numstat | gawk '{ add += $1 ; subs += $2 ; loc += $1 - $2 } END { printf "added lines: %s removed lines : %s total lines: %s\n",add,subs,loc }' -
@@ -30,10 +34,7 @@ if [ "$#" -eq 1 ] 2>/dev\null ;then
 	sleep 3
 
 	echo -e "\n统计项目中代码数据，项目比较大的话，请骚等，统计需要时间（需要安装cloc）\n结果如下：\n"
-	cloc $1/ 
+	cloc $path/ 
 
-	cd -
-else
-	echo -e "请输入项目所在路径~!\n"
 fi
 
